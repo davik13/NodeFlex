dce1a4ea612a78311b821ce85dc0519374b63525
LeoRouillard <leorouillard@gmail.com>
2022-03-07 12:04:23 +0100

    ajout method create, delete, findOne pour movies

--- back/routes/movies.js
+++ back/routes/movies.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const movieController = require('../controllers/movieController');
 
-router.get('/', movieController.findAll)
+router.get('/', movieController.findAll);
+router.get('/:id', movieController.findOne);
+router.post('/', movieController.create);
+router.delete('/:id', movieController.delete);
 
 module.exports = router;
