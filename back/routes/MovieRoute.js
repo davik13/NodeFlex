0dbe18c1164c05a0abd19d3b8d645ddecd72578f
Aimé Bea <72194058+davik13@users.noreply.github.com>
2022-03-10 14:24:09 +0100

    crud movie update

--- back/routes/MovieRoute.js
+++ back/routes/MovieRoute.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router();
+const verify = require('../middleware/jwtTokenVerify')
 
 const MovieController = require('../controllers/movieController');
 
-router.get('/', MovieController.findAll);
-router.get('/:id', MovieController.findOne);
-router.post('/create', MovieController.create);
-router.delete('/:id', MovieController.delete);
-router.put('/:id', MovieController.update);
+router.get('/',verify, MovieController.findAll);
+router.get('/:id',verify, MovieController.findOne);
+router.post('/create',verify, MovieController.create);
+router.delete('/:id',verify, MovieController.delete);
+router.put('/:id',verify, MovieController.update);
+router.get('/randomMovie', verify, MovieController.randomMovie);
 
 module.exports = router;
