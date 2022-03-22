d0f17dffb96154c84ea1cf4a71f7329850c27d6b
Aimé Bea <72194058+davik13@users.noreply.github.com>
2022-03-22 22:31:46 +0100

    fix

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,51 +4,24 @@ import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-do
 import {useContext} from "react"
 
 function App() {
-  const { user } = useContext(AuthContext);
   return (
-   <Router>
-     <Routes>
-       <Route path="/login">{user ? <Navigate to="/" /> : <Login />}</Route>
-       {user && (
-         <>
-            <Topbar />
-            <div className="container">
-              <Sidebar />
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/users">
-                <UserList />
-              </Route>
-              <Route path="/user/:userId">
-                <User />
-              </Route>
-              <Route path="/newUser">
-                <NewUser />
-              </Route>
-              <Route path="/movies">
-                <MovieList />
-              </Route>
-              <Route path="/movie/:movieId">
-                <Movie />
-              </Route>
-              <Route path="/newMovie">
-                <NewMovie />
-              </Route>
-              <Route path="/lists">
-                <ListList />
-              </Route>
-              <Route path="/list/:listId">
-                <List />
-              </Route>
-              <Route path="/newlist">
-                <NewList />
-              </Route>
-            </div>
-         </>
-       )}
-     </Routes>
-   </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Hello, world
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+   
   );
 }
 
